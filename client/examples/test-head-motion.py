bb4c2651d02b57ac9b5644e1ae90a3a25edf3bac
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2024-12-06 14:55:29 +0100

    Fix config import

--- client/examples/test-head-motion.py
+++ client/examples/test-head-motion.py
@@ -1,8 +1,19 @@
 # -*- encoding: UTF-8 -*-
 
+import os
+import sys
+
+# Locate the config file dynamically
+config_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, config_dir)
+
 ''' Whole Body Motion: Head orientation control '''
 ''' This example is only compatible with NAO '''
+
+# TODO: Fix problem with example test-head-motion.py
+
 from config import *
+
 import time
 import math
 from naoqi3 import ALProxy
--- client/examples/test-say-hello.py
+++ client/examples/test-say-hello.py
@@ -1,10 +1,19 @@
 # main.py (Python 3)
+import os
+import sys
+
+# Locate the config file dynamically
+config_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, config_dir)
+
+
 from config import *
 from naoqi3 import ALProxy
 
-IP = '192.168.0.122'
 tts = ALProxy("ALTextToSpeech", IP, PORT)
 tts.say("Ahoj, jsem Nao.")
+tts.say("Ja jsem robot.")
+tts.say("Ja pracuju, aby lidi mohli vic relaxovat.")
 
 # motionProxy = ALProxy("ALMotion", IP, PORT)
 # motionProxy.rest()
