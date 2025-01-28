9bbd57cd208e7ffd04ca03aba2c440620ff3db29
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2025-01-28 12:13:25 +0100

    Add coding specification

--- server/examples/test-say.py
+++ server/examples/test-say.py
@@ -1,8 +1,6 @@
+# -*- coding: utf-8 -*-
 from naoqi import ALProxy
 
 IP = '192.168.0.122'
 tts = ALProxy("ALTextToSpeech", IP, 9559)
 tts.say("Ahoj, jsem Nao.")
-
-# motionProxy = ALProxy("ALMotion", IP, 9559)
-# motionProxy.rest()
