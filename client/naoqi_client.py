2b5db1401966dceaca664e03b13f7d41bc2fe370
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2025-02-07 14:12:03 +0100

    start fixing image decoding issue

--- client/naoqi_client.py
+++ client/naoqi_client.py
@@ -1,6 +1,8 @@
 # naoqi_client.py (Python 3)
 import socket
 import pickle
+import struct
+
 
 class ALProxyWrapper:
     def __init__(self, module, ip, port):
@@ -26,8 +28,35 @@ class ALProxyWrapper:
                 s.sendall(pickle.dumps(command, protocol=2))  # Use protocol 2 for Python 2 compatibility
 
                 # Receive the response
-                response = s.recv(4096)
-                result = pickle.loads(response)
+                # response = s.recv(4096)
+                # result = pickle.loads(response)
+                # data = bytearray()
+                # while True:
+                #     response = s.recv(4096)
+                #     if not response: break
+                #     data.extend(response)
+                # result = pickle.loads(bytes(data))
+
+                # Přijetí velikosti dat (4 bajty)
+                size_data = s.recv(4)
+                if len(size_data) < 4:
+                    raise RuntimeError("Failed to receive data size")
+
+                data_size = struct.unpack('>I', size_data)[0]
+                print("Expecting", data_size, "bytes")
+
+                # Přijetí celé zprávy
+                data = b''
+                while len(data) < data_size:
+                    packet = s.recv(min(4096, data_size - len(data)))
+                    if not packet:
+                        raise RuntimeError("Connection closed before all data was received")
+                    data += packet
+                    print(f"Data length: {len(data)}")
+
+                print("Received", len(data), "bytes")
+                result = pickle.loads(data)
+                print(f"RESULT: {result}\n\n")
                 
                 # Check if the response contains an error
                 if not result['success']:
