26fea353c477448c8909ce18d77b7a82bcd76cac
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2025-03-07 12:35:53 +0100

    Fix bug with displaying text on img

--- client/vision-examples/fingers_recognitions.py
+++ client/vision-examples/fingers_recognitions.py
@@ -22,32 +22,29 @@ def get_image(cam_proxy):
     """
     Pošle požadavek na získání snímku z kamery NAO a vrátí jej jako OpenCV obrázek.
     """
-    resolution = vision_definitions.kQVGA  # 320x240
-    color_space = vision_definitions.kBGRColorSpace  # OpenCV používá BGR
-
-    nameId = cam_proxy.subscribe("python_GVM", resolution, color_space, 30)
     image = cam_proxy.getImageRemote(nameId)
-    cam_proxy.unsubscribe(nameId)
 
     if image is None:
         return None
 
     width = image[0]
     height = image[1]
-    array = np.frombuffer(bytes(image[6], encoding='latin1'), dtype=np.uint8).reshape((height, width, 3))
-    return array
+    channels = image[2]
+    array = np.frombuffer(bytes(image[6], encoding='latin1'), dtype=np.uint8).reshape((height, width, channels))
+    return array.copy()
+
 
 def count_fingers(image):
-    processed_img = hand_detector.find_hands(image)
-    land_mark_list = hand_detector.find_position(processed_img, draw=False)
+    image = hand_detector.find_hands(image)
+    land_mark_list = hand_detector.find_position(image, draw=False)
     fingers_up = hand_detector.fingers_up()
 
     if fingers_up is not None:
         fingers_up_count = sum(fingers_up)
     else:
-        fingers_up_count = 0
+        fingers_up_count = None
 
-    return fingers_up_count, processed_img
+    return fingers_up_count
 
 
 if __name__ == "__main__":
@@ -60,23 +57,27 @@ if __name__ == "__main__":
     tts = ALProxy("ALTextToSpeech", IP, PORT)
     hand_detector = HandDetector()
 
-    # get images from robot camera and count fingers
-    i = 1
-    while True:
-        frame = get_image(cam_proxy)
-        if frame is None:
-            print("Nepodařilo se získat snímek.")
-            continue
-    
-        fingers, landmark_img = count_fingers(frame)
+    resolution = vision_definitions.kQVGA  # 320x240
+    color_space = vision_definitions.kBGRColorSpace  # OpenCV používá BGR
 
-        # save img 
-        image_path = os.path.join(image_folder, str(i).zfill(3) + "image_" + str(fingers) + "fingers" + ".png")
-        cv2.imwrite(image_path, landmark_img)
-        print("Saved: ", image_path)
+    nameId = cam_proxy.subscribe("python_GVM", resolution, color_space, 30)
 
-        # say fingers number
-        tts.say("Vidim {} prstu.".format(fingers))
+    try:
+        # get images from robot camera and count fingers
+        while True:
+            frame = get_image(cam_proxy)
+            if frame is None:
+                print("Nepodařilo se získat snímek.")
+                continue
+        
+            fingers = count_fingers(frame)
+            cv2.putText(frame, f"Fingers up: {str(fingers)}", (50, 50),  cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 0, 255), 1)
 
-        time.sleep(2)  # sleep for a while
-        i+=1
+            cv2.imshow('image', frame)
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+
+    except Exception as e:
+        print(e)
+    finally:
+        cam_proxy.unsubscribe(nameId)
