f5f74fe64342345a75353ce88b77ae2a092e02ec
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2025-04-04 13:38:47 +0200

    implement albroker class

--- client/naoqi3.py
+++ client/naoqi3.py
@@ -1,8 +1,9 @@
 # naoqi3.py (Python 3)
-from naoqi_client import ALProxyWrapper
+from naoqi_client import ALProxyWrapper, ALBrokerWrapper
+
 
 class ALProxy:
-    def __init__(self, module_name, ip, port):
+    def __init__(self, module_name, ip=None, port=None):
         self._proxy = ALProxyWrapper(module_name, ip, port)
 
     def __getattr__(self, method_name):
@@ -11,8 +12,8 @@ class ALProxy:
 
 class ALBroker:
     def __init__(self, name, ip, port, parent_ip, parent_port):
-        ...
+        self._broker = ALBrokerWrapper(name, ip, port, parent_ip, parent_port)
+        self.init_result = self._broker.init_broker()
 
-
-class ALModule:
-    pass
+    def __getattr__(self, method_name):
+        return getattr(self._broker, method_name)
